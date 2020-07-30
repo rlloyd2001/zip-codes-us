f47d16d3981aff59fe58cbc5b8dd74512615b48d
Ron Lloyd <rlloyd2001@gmail.com>
2020-07-30 00:05:23 -0600

    adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+    },
+    "extends": "eslint:recommended",
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
