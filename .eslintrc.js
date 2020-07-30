2f44ae5766cc56b73dce609399e74e92a50a5270
Ron Lloyd <rlloyd2001@gmail.com>
2020-07-30 00:09:49 -0600

    fixing lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-    },
-    "extends": ["eslint:recommended", "plugin:prettier/recommended"],
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {},
 };
